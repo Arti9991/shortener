c2b62c1c2eb7acdf22880603224680855bcd823e
Артем Анисимов <arti99.an99@mail.ru>
2024-11-22 20:39:13 +0300

    Add server and first request for '/' POST

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/Arti9991/shortener/internal/app/handlers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handlers.MainPage)
+	mux.HandleFunc(`/api/`, handlers.ApiPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
