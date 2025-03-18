d1150fe85e34b195279b559717d7ccd243372a75
Артем Анисимов <arti99.an99@mail.ru>
2025-03-19 01:11:14 +0300

    Turned off databse for storage tests

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/Arti9991/shortener/internal/models"
-	"github.com/Arti9991/shortener/internal/storage/database"
 	"github.com/Arti9991/shortener/internal/storage/files"
 	"github.com/Arti9991/shortener/internal/storage/inmemory"
 	"github.com/stretchr/testify/assert"
@@ -13,8 +12,8 @@ import (
 )
 
 func BenchmarkStor(b *testing.B) {
-	DBInterface, err := database.DBinit("host=localhost user=myuser password=123456 dbname=ShortURL sslmode=disable")
-	require.NoError(b, err)
+	//DBInterface, err := database.DBinit("host=localhost user=myuser password=123456 dbname=ShortURL sslmode=disable")
+	//require.NoError(b, err)
 
 	FileInterface, err := files.NewFiles("./test_file.csv")
 	require.NoError(b, err)
@@ -32,21 +31,21 @@ func BenchmarkStor(b *testing.B) {
 	}
 
 	//fmt.Printf("\n%s", hashStr)
-	b.Run("STORAGE in database save", func(b *testing.B) {
-		for i := 0; i < numReps; i++ {
-			DBInterface.Save(hashStr[i], URLStr[i], UserID)
-		}
-	})
+	// b.Run("STORAGE in database save", func(b *testing.B) {
+	// 	for i := 0; i < numReps; i++ {
+	// 		DBInterface.Save(hashStr[i], URLStr[i], UserID)
+	// 	}
+	// })
 
-	b.Run("STORAGE in database get", func(b *testing.B) {
-		for i := 0; i < numReps; i++ {
-			res, err := DBInterface.Get(hashStr[i])
-			require.NoError(b, err)
-			assert.Equal(b, res, URLStr[i])
-		}
-	})
-	err = DBInterface.DropTable()
-	require.NoError(b, err)
+	// b.Run("STORAGE in database get", func(b *testing.B) {
+	// 	for i := 0; i < numReps; i++ {
+	// 		res, err := DBInterface.Get(hashStr[i])
+	// 		require.NoError(b, err)
+	// 		assert.Equal(b, res, URLStr[i])
+	// 	}
+	// })
+	// err = DBInterface.DropTable()
+	// require.NoError(b, err)
 
 	b.Run("STORAGE in memory save", func(b *testing.B) {
 		for i := 0; i < numReps; i++ {
