53c1fef939a26bbda916f4c41d44d33f48cfbade
Artem An <Arti An>
2025-04-02 18:19:04 +0300

    Added global vriables with version, date and commit info

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/Arti9991/shortener/internal/server"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	// запуск сервера со всеми настройками
 	err := server.RunServer()
 	if err != nil {
